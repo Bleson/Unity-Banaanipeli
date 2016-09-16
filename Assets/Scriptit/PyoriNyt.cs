e1bae0266dd2a8c7f69fd18b845158f6eb8c7f8b
Koivisto Sami PAL16K <samikoivisto@kamk.fi>
2016-09-17 00:46:27 +0300

    Banaaneja Kaikkialla

---
+++ Assets/Scriptit/PyoriNyt.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PyoriNyt : MonoBehaviour
+{
+    Vector2 suunta;
+    public float nopeus = 10f;
+
+    void Start()
+    {
+        suunta = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward);
+        transform.localPosition = new Vector2 (transform.localPosition.x + suunta.x * nopeus * Time.deltaTime, transform.localPosition.y + suunta.y * nopeus * Time.deltaTime);
+        TarkistaRajat();
+    }
+
+    void TarkistaRajat()
+    {
+        if (transform.localPosition.x > 1005)
+        {
+            float y = transform.localPosition.y;
+            transform.localPosition = new Vector2(-1000f, y);
+        }
+        else if (transform.localPosition.x < -1005)
+        {
+            float y = transform.localPosition.y;
+            transform.localPosition = new Vector2(1000, y);
+        }
+        else if (transform.localPosition.y > 535)
+        {
+            float x = transform.localPosition.x;
+            transform.localPosition = new Vector2(-530, x);
+        }
+        else if (transform.localPosition.y < -535)
+        {
+            float x = transform.localPosition.x;
+            transform.localPosition = new Vector2(530, x);
+        }
+
+    }
+}
---
+++ Assets/Scriptit/ValikkoSiilo.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ValikkoSiilo : MonoBehaviour
+{
+    [SerializeField] GameObject PaaValikko;
+    [SerializeField] GameObject LuottoRuutu;
+
+    void Start()
+    {
+        PaaValikko = transform.FindChild("Panel").FindChild("PaaValikko").gameObject;
+        LuottoRuutu = transform.FindChild("Panel").FindChild("LuottoRuutu").gameObject;
+
+        PaaValikko.gameObject.SetActive(true);
+    }
+
+    public void PolkaisePeliKayntiin()
+    {
+        Debug.Log("Olis varmaan kiva pelatakki jotain...");
+    }
+
+    public void SiirryPaaValikkoon()
+    {
+        PaaValikko.SetActive(true);
+        LuottoRuutu.SetActive(false);
+    }
+
+    public void SiirryLuottoRuutuun()
+    {
+        PaaValikko.SetActive(false);
+        LuottoRuutu.SetActive(true);
+    }
+
+    public void LopetaPeli()
+    {
+        Debug.Log("Hyvästi Tuulenpesä!");
+        Application.Quit();
+        
+    }
+
+}
