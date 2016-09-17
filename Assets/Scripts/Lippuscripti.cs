e80f006386c6b794735beb214295ec658726193e
Koivisto Sami PAL16K <samikoivisto@kamk.fi>
2016-09-17 10:29:23 +0300

    Pikkusta härpäkettä

---
+++ Assets/Scripts/Lippuscripti.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lippuscripti : MonoBehaviour
+{
+    Vector2 originalPosition;
+    Vector2 targetPosition;
+    float progress = 0;
+    float nopeus = 6;
+
+    void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            originalPosition = transform.position;
+            targetPosition = transform.position + new Vector3(0, 300);
+            progress = 0;
+        }
+
+        progress += Time.deltaTime * nopeus;
+        transform.position =  Vector2.Lerp(originalPosition, targetPosition, progress);
+    }
+}
