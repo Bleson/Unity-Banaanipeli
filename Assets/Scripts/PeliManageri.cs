a5ba8782c476072c3c4bc0bb7a594f6173cada01
Hyvärinen Markus TTK15SP <markushyvarinen@kamk.fi>
2016-09-18 15:46:17 +0300

    Picnic

--- Assets/Scripts/PeliManageri.cs
+++ Assets/Scripts/PeliManageri.cs
@@ -87,6 +87,7 @@ public class PeliManageri : Singleton<PeliManageri> {
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            //Application.LoadLevel(1);
             StartGame();
         }
 	}
